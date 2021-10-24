a195e2400f923646a52250f81cdcce9985bd5ac1
arman <arman.seisenbek@gmail.com>
2021-10-24 17:01:19 +0600

    user authorization service lombok

--- user-authorization-service/src/main/java/com/devteam/userauthorizationservice/model/RegisteredUser.java
+++ user-authorization-service/src/main/java/com/devteam/userauthorizationservice/model/RegisteredUser.java
@@ -1,5 +1,12 @@
 package com.devteam.userauthorizationservice.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class RegisteredUser {
 
     private String username;
@@ -7,26 +14,6 @@ public class RegisteredUser {
 
     public RegisteredUser(String username, String password) {
         this.username = username;
-        this.password = password;
-    }
-
-    public RegisteredUser() {
-
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
--- user-authorization-service/src/main/java/com/devteam/userauthorizationservice/model/RegisteredUsers.java
+++ user-authorization-service/src/main/java/com/devteam/userauthorizationservice/model/RegisteredUsers.java
@@ -1,24 +1,19 @@
 package com.devteam.userauthorizationservice.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class RegisteredUsers {
 
     private List<RegisteredUser> registeredUserList;
 
     public RegisteredUsers(List<RegisteredUser> registeredUserList) {
-        this.registeredUserList = registeredUserList;
-    }
-
-    public RegisteredUsers() {
-
-    }
-
-    public List<RegisteredUser> getRegisteredUserList() {
-        return registeredUserList;
-    }
-
-    public void setRegisteredUserList(List<RegisteredUser> registeredUserList) {
         this.registeredUserList = registeredUserList;
     }
 }
