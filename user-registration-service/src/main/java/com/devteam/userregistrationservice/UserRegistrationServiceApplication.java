46bdb7da3112726061e28e204359838ce74f6a01
arman <arman.seisenbek@gmail.com>
2021-10-24 17:28:52 +0600

    user-registration-service and user-authorization-service swagger configuration

--- user-registration-service/src/main/java/com/devteam/userregistrationservice/UserRegistrationServiceApplication.java
+++ user-registration-service/src/main/java/com/devteam/userregistrationservice/UserRegistrationServiceApplication.java
@@ -4,13 +4,13 @@ import com.devteam.userregistrationservice.service.UserRegistrationService;
 import com.devteam.userregistrationservice.service.UserRegistrationServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class UserRegistrationServiceApplication {
 
 	public static void main(String[] args) {
