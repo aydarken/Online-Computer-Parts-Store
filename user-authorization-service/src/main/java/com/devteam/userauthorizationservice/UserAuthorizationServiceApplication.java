8b56f19501a19366059d51375f6041e7d6c9813c
arman <arman.seisenbek@gmail.com>
2021-10-26 16:49:14 +0600

    user registration service, user authorization service Hystrix

--- user-authorization-service/src/main/java/com/devteam/userauthorizationservice/UserAuthorizationServiceApplication.java
+++ user-authorization-service/src/main/java/com/devteam/userauthorizationservice/UserAuthorizationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
+@EnableHystrix
 public class UserAuthorizationServiceApplication {
 
 	public static void main(String[] args) {
--- user-registration-service/src/main/java/com/devteam/userregistrationservice/controller/UserRegistrationController.java
+++ user-registration-service/src/main/java/com/devteam/userregistrationservice/controller/UserRegistrationController.java
@@ -1,5 +1,6 @@
 package com.devteam.userregistrationservice.controller;
 
+import com.devteam.userregistrationservice.model.RegisteredUser;
 import com.devteam.userregistrationservice.model.RegisteredUsers;
 import com.devteam.userregistrationservice.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("registration")
@@ -16,13 +20,25 @@ public class UserRegistrationController {
     UserRegistrationService userRegistrationService;
 
     @RequestMapping("/register/{username}/{password}")
+    @HystrixCommand(fallbackMethod = "registerUserFallback")
     public void registerUser(@PathVariable("username") String username, @PathVariable("password") String password) {
         userRegistrationService.registerUser(username, password);
     }
 
+    public void registerUserFallback(String username, String password) {
+        System.out.println("User not registered: Service Unavailable");
+    }
+
     @GetMapping("/registered_users")
+    @HystrixCommand(fallbackMethod = "getAllRegisteredUsersFallback")
     public RegisteredUsers getAllRegisteredUsers() {
         return new RegisteredUsers(userRegistrationService.getAllRegisteredUsers());
     }
+
+    public RegisteredUsers getAllRegisteredUsersFallback() {
+        return new RegisteredUsers(new ArrayList<RegisteredUser>());
+    }
+
+
 
 }
