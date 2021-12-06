925f11879a7d7122294f0213040c96dc460fad08
Aidarken Nurbek <64406796+aydarken@users.noreply.github.com>
2021-12-06 23:21:57 +0600

    Update UserRefundController.java

--- user-refund-service/src/main/java/com/devteam/userrefundservice/controller/UserRefundController.java
+++ user-refund-service/src/main/java/com/devteam/userrefundservice/controller/UserRefundController.java
@@ -1,24 +1,24 @@
 package com.devteam.userrefundservice.controller;
 
-import com.devteam.userrefundservice.model.OrderItem;
+
+import com.devteam.userrefundservice.repository.OrderRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/refund")
 public class UserRefundController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Autowired private OrderRepository orderRepository;
 
-    @RequestMapping("/{username}/{partId}")
+    @RequestMapping(value = "/{orderId}", method = RequestMethod.POST)
     @HystrixCommand(
             fallbackMethod = "refundOrderFallback",
             threadPoolKey = "refundOrder",
@@ -27,21 +27,18 @@ public class UserRefundController {
                     @HystrixProperty(name = "maxQueueSize", value = "50"),
             }
     )
-    public void refundOrder(
-            @PathVariable String username,
-            @PathVariable String partId
+    public ResponseEntity<?> refundOrder(
+            @PathVariable("orderId") Long orderId
     ) {
-
-            List<OrderItem> orderedItems = restTemplate
-                    .getForObject("http://user-orders-service/user-orders/orders/" + username, List.class);
-
-            if (orderedItems != null) {
-                restTemplate.delete("http://user-orders-service/user-orders/orders/remove/" + partId);
-            }
-
+        try {
+            orderRepository.deleteById(orderId);
+            return ResponseEntity.ok("Order refunded successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
 
-    public void refundOrderFallback(String username, String partId){
-        System.out.println("User orders is not available");
+    public ResponseEntity<?> refundOrderFallback(Long orderId){
+        return ResponseEntity.badRequest().body("User orders is not available");
     }
 }
