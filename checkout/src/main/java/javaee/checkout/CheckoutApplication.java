2468e6bcde73d5ba15b79a8897b906f35a38c978
Anuar077 <70533690+Anuar077@users.noreply.github.com>
2021-12-06 23:10:50 +0600

    Update CheckoutApplication.java

--- checkout/src/main/java/javaee/checkout/CheckoutApplication.java
+++ checkout/src/main/java/javaee/checkout/CheckoutApplication.java
@@ -2,6 +2,7 @@ package javaee.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,7 @@ public class CheckoutApplication {
 	}
 
 	@Bean
+	@LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
