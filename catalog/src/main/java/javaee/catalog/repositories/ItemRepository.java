b0d5e113cfca442d7e286043c9d065a228cb44e8
Anuar077 <70533690+Anuar077@users.noreply.github.com>
2021-12-06 23:08:38 +0600

    Delete ItemRepository.java

--- catalog/src/main/java/javaee/catalog/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package javaee.catalog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javaee.catalog.models.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-} 
