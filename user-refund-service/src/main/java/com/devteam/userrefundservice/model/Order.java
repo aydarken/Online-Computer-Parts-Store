488fcd8ba6e852c833c1b911f9704581171f9d70
Aidarken Nurbek <64406796+aydarken@users.noreply.github.com>
2021-12-06 23:22:51 +0600

    Update and rename OrderItem.java to Order.java

---
+++ user-refund-service/src/main/java/com/devteam/userrefundservice/model/Order.java
@@ -0,0 +1,18 @@
+package com.devteam.userrefundservice.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private Long itemId;
+}
