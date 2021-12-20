98de380a8e86b334249934545101b5faed2330ee
Aidarken Nurbek <64406796+aydarken@users.noreply.github.com>
2021-12-20 20:15:36 +0600

    Delete user-reviews-service/user-reviews-service directory

--- user-reviews-service/user-reviews-service/src/main/java/com/devteam/userreviewsservice/repositories/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devteam.userreviewsservice.repositories;
-
-import com.devteam.userreviewsservice.models.ReviewItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<ReviewItem, Long> {
-}
